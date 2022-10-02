eab3dd26bd955c356f9c5bf849f76154a246eabe
Yuuuusssuufff <ajibolayusuf8@gmail.com>
2022-10-02 23:42:55 +0100

    init commit

--- index.js
+++ index.js
@@ -1,34 +1,19 @@
+const persons = require("./persons");
 
-const http = require("http");
-const server = http.createServer;
+// const { response } = require("express");
+const express = require("express");
+const app = express();
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2022-05-30T17:30:31.098Z",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2022-05-30T18:39:34.091Z",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2022-05-30T19:20:14.298Z",
-    important: true
-  }
-]
 
-const app = server((req, res) => {
-  res.writeHead(200, { "Content-Type": "application/json" });
-  res.end(JSON.stringify(notes));
+app.get("/", (request, response) => {
+  response.send("<h1>Hello World!!!</h1>");
 });
 
-const PORT = 5000;
-app.listen(PORT);
-console.log(`app is running on port ${PORT}`);
-
+app.get("/notes", (request, response) => {
+  response.json(persons);
+});
+app.get("/part", (request, response) =>
+  response.send("<h2>This is part 1</h2>")
+);
+const PORT = 3001;
+app.listen(PORT, () => console.log("app is running on 3003"));
