7a57506c7d1ac8940484f2e1e217c94b92a6e7bd
Yuuuusssuufff <ajibolayusuf8@gmail.com>
2022-10-08 07:18:44 +0100

    init commit

--- index.js
+++ index.js
@@ -1,19 +1,54 @@
 const persons = require("./persons");
+const cors = require('cors')
 
 // const { response } = require("express");
 const express = require("express");
 const app = express();
-
+app.use(cors())
 
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!!!</h1>");
 });
 
-app.get("/notes", (request, response) => {
+app.get("/persons", (request, response) => {
   response.json(persons);
 });
-app.get("/part", (request, response) =>
-  response.send("<h2>This is part 1</h2>")
+app.get("/info", (request, response) =>
+  response.send(
+    `<p>Phonebook has info for ${persons.length} people</p><p>${new Date()}</p>`
+  )
 );
-const PORT = 3001;
+
+app.get("/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const person = persons.find((person) => person.id === id);
+  
+  if(!person){response.status(400).json({error: 'content missing'})}
+  else{response.json(person)}
+});
+
+app.delete('/persons/:id', (request, response)=>{
+  const id = Number(request.params.id)
+  const persons = persons.filter(person => person.id !== id)
+  response.status(204).end()
+})
+
+
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log("app is running on 3003"));
+
+// const os = require('os')
+// const user = os.userInfo()
+// console.log(user)
+// console.log(os.uptime())
+// const currentOs = {
+//   name: os.type(),
+//   release: os.release(),
+//   totalMem: os.totalmem(),
+//   freeMem: os.freemem()
+// }
+
+// console.log(currentOs)
+
+// const path = require('path')
+// console.log(path.sep)
