ca7a73e01104e9457d719eb871066a8bd148f1e4
Yusuf-Ajibola <ajibolayusuf8@yahoo.com>
2022-12-26 03:14:04 +0100

    chaptechapter 3 init

--- index.js
+++ index.js
@@ -1,54 +1,91 @@
-const persons = require("./persons");
-const cors = require('cors')
+let persons = require("./persons");
+const cors = require("cors");
 
-// const { response } = require("express");
+const { response, application } = require("express");
+
 const express = require("express");
 const app = express();
-app.use(cors())
 
-app.get("/", (request, response) => {
+app.use(express.json());
+app.use(cors());
+app.use(express.static("build"));
+
+const requestLogger = (request, response, next)=>{
+  console.log('METHOD', request.method)
+  console.log('PATH:', request.path)
+  console.log('Body:', request.body)
+  console.log("-----")
+  next()
+}
+
+app.use(requestLogger)
+
+const generateId = () => {
+  const maxId = persons.length > 0 ? Math.max(...persons.map((p) => p.id)) : 0;
+  return maxId + 1;
+};
+
+app.get("/api", (request, response) => {
   response.send("<h1>Hello World!!!</h1>");
 });
 
-app.get("/persons", (request, response) => {
+app.get("/api/persons", (request, response) => {
   response.json(persons);
 });
-app.get("/info", (request, response) =>
+
+app.get("/api/info", (request, response) =>
   response.send(
     `<p>Phonebook has info for ${persons.length} people</p><p>${new Date()}</p>`
   )
 );
 
-app.get("/persons/:id", (request, response) => {
+app.get("/api/persons/:id", (request, response) => {
   const id = Number(request.params.id);
   const person = persons.find((person) => person.id === id);
-  
-  if(!person){response.status(400).json({error: 'content missing'})}
-  else{response.json(person)}
+
+  if (!person) {
+    response.status(400).json({ error: "content missing" });
+  } else {
+    response.json(person);
+  }
 });
 
-app.delete('/persons/:id', (request, response)=>{
-  const id = Number(request.params.id)
-  const persons = persons.filter(person => person.id !== id)
-  response.status(204).end()
-})
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter((person) => person.id !== id);
+  response.status(204).end();
+});
 
+app.post("/api/persons", (request, response) => {
+  const person = request.body;
+  // response.setHeader("Content-Type", "application/json")
+  person.id = generateId();
+  // person.name = "Olanrewaju Opeyemi";
+  // person.number = "081-081-676-28";
 
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => console.log("app is running on 3003"));
+  !person.name
+    ? response.status(400).json({ error: "name is missing" })
+    : person.name;
+  !person.number
+    ? response.status(400).json({ error: "number is missing" })
+    : person.number;
+    if(persons.filter(p => p.name === person.name)){
+      response.status(400).json({ error: "name must be unique" })
+    }
+  // persons.filter(p => p.name === person.name)
+  //   ? response.status(400).json({ error: "name must be unique" })
+  //   : person.name;
 
-// const os = require('os')
-// const user = os.userInfo()
-// console.log(user)
-// console.log(os.uptime())
-// const currentOs = {
-//   name: os.type(),
-//   release: os.release(),
-//   totalMem: os.totalmem(),
-//   freeMem: os.freemem()
-// }
+  persons = persons.concat(person);
+  response.json(person);
+});
 
-// console.log(currentOs)
+const invalidEndPoint = (request, response) => {
+  response.setHeader('Content-Type', 'application/json')
+  response.status(404).send({error: 'invalid route'})
+}
+app.use(invalidEndPoint)
 
-// const path = require('path')
-// console.log(path.sep)
+const PORT = 3001;
+// const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => console.log(`app is running on this ${PORT}`));
